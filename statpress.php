3afb242c1b0cc96a7f1e77ad9de25a93806cfbaa
danielelippi <danielelippi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-12 16:46:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/statpress/trunk@182105 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- statpress.php
+++ statpress.php
@@ -3,7 +3,7 @@
 Plugin Name: StatPress
 Plugin URI: http://forum.irisco.it/forum.php?id=1
 Description: Real time stats for your Wordpress blog
-Version: 1.3.2
+Version: 1.3.3
 Author: Daniele Lippi
 Author URI: http://www.irisco.it
 */
